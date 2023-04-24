31204ea1bd686133fd2ed5404499e5ef7670fb2a
milk-maid <roqbell@gmail.com>
2023-04-24 14:35:40 +0100

    100 days of Python. Day Three Projects

---
+++ day3_ControlFlow&LogicalOperators/treasure_island.py
@@ -0,0 +1,47 @@
+print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+
+# https://ascii.co.uk/art/
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[Game!!]
+*******************************************************************************
+''')
+
+direction = input("You're at a cross road. Where do you want to go? Type \"Left\" or \"Right\" \n")
+
+# if direction == "right" or direction == "RIGHT" or direction == "Right":
+#     print("Wrong Direction\nGame Over!!!")
+if direction == "left" or direction == "LEFT" or direction == "Left":
+    movement = input("You come to a lake. There is an Island in the middle of the lake. Type \"wait\" to wait for "
+                     "a boat. Type \"swim\" to swim across.\n")
+    if movement == "swim" or movement == "Swim" or movement == "SWIM":
+        print("That's a wrong choice\nGame Over!!!")
+    elif movement == "wait" or movement == "Wait" or movement == "WAIT":
+        door = input("You arrived at the island unharmed. This is a house with 3 doors. red, yellow and blue. Which "
+                     "color do you choose?\n")
+        if door == "red" or door == "blue" or door == "Red" or door == "Blue":
+            print("The Wrong Door\nGame Over!!!")
+        elif door == "yellow" or door == "Yellow" or door == "YELLOW":
+            print("WOW! Congratulations you found it...\nYou Win!!!")
+else:
+    print("Wrong Direction\nGame Over!!!")
+
+print("THE END")
